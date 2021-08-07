28bba3c802dcab6137697e0c5aadda97e02a4039
Nich87 <56439343+Nich87@users.noreply.github.com>
2021-08-07 14:27:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ http
     })
     .listen(8080);
 const Discord = require('discord.js');
-const client = new Discord.Client({
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
-});
+const client = new Discord.Client();
 const config = require('./config.json');
 
 const ytdl = require('ytdl-core');
